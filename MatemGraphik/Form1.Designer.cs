dcab4757b4a05e765a5ae9eceba357a8a8f4acb1
babya <babya@ERIC_WEBER>
2022-07-01 13:15:35 +0300

    Add project files.

---
+++ MatemGraphik/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace MatemGraphik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Линейная",
+            "Квадратичная",
+            "Логорефмичная"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(269, 14);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(35, 20);
+            this.maskedTextBox2.TabIndex = 2;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(191, 14);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(35, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(704, 482);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Textovic";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ MatemGraphik/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Media;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MatemGraphik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+        double xo = 0, yo = 0;
+        void print_pole()
+        {
+            Graphics g = CreateGraphics();
+            Pen pen = new Pen(Color.Black, 5.0f);
+            g.DrawLine(pen, 300, 400, 300, 100);
+            g.DrawLine(pen, 450, 250, 150, 250);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            g.Clear(Color.FromArgb(64, 64, 64));
+            print_pole();
+            Pen pen = new Pen(Color.Blue, 5.0f);
+            double k = 0, b = 0;
+            if (comboBox1.SelectedIndex == 0 && maskedTextBox1.Text != "" && maskedTextBox2.Text != "")
+            {
+                double x = 0, y = 0;
+                k = double.Parse(maskedTextBox1.Text);
+                b = double.Parse(maskedTextBox2.Text) * 10;
+                for (int i = -150; i <= 150; i++)
+                {
+                    if (i == -150)
+                    {
+                        x = i;
+                        y = k * x + b;
+                    }
+                    else
+                    {
+                        float x1 = (float)(x + xo), y1 = (float)((y * -1) + yo), x2 = (float)(i + xo), y2 = (float)((((k * i) + b) * -1) + yo);
+                        g.DrawLine(pen, x1, y1, x2, y2);
+                        x = i;
+                        y = k * x + b;
+                    }
+                }
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                double x = 0, y = 0;
+                for (int i = -150; i <= 150; i++)
+                {
+
+                    if (i == -150)
+                    {
+                        x = i;
+                        y = Math.Pow(x, 2);
+                    }
+                    else
+                    {
+                        float x1 = (float)(x + xo), y1 = (float)((y * -1) + yo), x2 = (float)(i + xo), y2 = (float)((Math.Pow(x, 2) * -1) + yo);
+                        g.DrawLine(pen, x1, y1, x2, y2);
+                        x = i;
+                        y = Math.Pow(x, 2);
+                    }
+                }
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                double x = 0, y = 0;
+                double a = double.Parse(maskedTextBox1.Text);
+                for (int i = 1; i < 150; i++)
+                {
+
+                    if (i == 1)
+                    {
+                        x = i;
+                        y = Math.Log(x) * a;
+                    }
+                    else
+                    {
+                        float x1 = (float)(x + xo), y1 = (float)((y * -1) + yo), x2 = (float)(i + xo), y2 = (float)(((Math.Log(x) * a) * -1) + yo);
+                        g.DrawLine(pen, x1, y1, x2, y2);
+                        x = i;
+                        y = Math.Log(x) * a;
+                    }
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                maskedTextBox1.Visible = true; maskedTextBox1.Text = "";
+                maskedTextBox2.Visible = true; maskedTextBox2.Text = "";
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                maskedTextBox1.Visible = false; maskedTextBox1.Text = "";
+                maskedTextBox2.Visible = false; maskedTextBox2.Text = "";
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                maskedTextBox1.Visible = true; maskedTextBox1.Text = "";
+                maskedTextBox2.Visible = false; maskedTextBox2.Text = "";
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            print_pole();
+            xo = 300;
+            yo = 250;
+        }
+    }
+}
